192c3c768d6addfd54e2eec3cba87abaec374cf2
jwnardine <jwnardine@gmail.com>
2016-02-19 10:31:51 -0800

    add content to app.php, including routes to form.html.twig and results.html.twig, as well as connecting to yet-to-be-written form inputs 'word' and 'sentence'

---
+++ app/app.php
@@ -0,0 +1,17 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
+    $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('form.html.twig');
+    });
+    $app->get("/view_results", function() use($app) {
+        $my_RepeatCounter = new RepeatCounter;
+        $check_results = $my_RepeatCounter->countRepeats($_GET['word'], $_GET['sentence']);
+        return $app['twig']->render('results.html.twig', array('result' => $check_results));
+    });
+    return $app;
+?>
