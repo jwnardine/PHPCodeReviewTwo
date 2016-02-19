617f9e721b5f2f194e0ee779e88088a7e2c202ed
jwnardine <jwnardine@gmail.com>
2016-02-19 10:40:35 -0800

    create form in which user will input word and sentence, link app.php to form

--- app/app.php
+++ app/app.php
@@ -8,7 +8,7 @@
     $app->get("/", function() use ($app) {
         return $app['twig']->render('form.html.twig');
     });
-    $app->get("/view_results", function() use($app) {
+    $app->get("/word_sentence", function() use($app) {
         $my_RepeatCounter = new RepeatCounter;
         $check_results = $my_RepeatCounter->countRepeats($_GET['word'], $_GET['sentence']);
         return $app['twig']->render('results.html.twig', array('result' => $check_results));
